eac2ba35560b2b23dbb50c7f8638114c6485131e
Prachi Gupta <prachiguptage@gmail.com>
2018-09-01 22:04:22 -0700

    adding test in Initial enteries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -101,6 +101,16 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */   
+         beforeEach(function(done){
+            loadFeed(0,function(){
+                done();
+            });
+         });
+         it('Complete Loading',function(done){
+            const feed= document.querySelector('.feed');
+            expect(feed.children.length>0).toBe(true);
+            done();
+         });
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
