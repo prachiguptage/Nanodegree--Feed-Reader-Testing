f363814f517e196de93558680bce0b85d0890436
Prachi Gupta <prachiguptage@gmail.com>
2018-09-01 20:36:00 -0700

    test suite for Name

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -45,6 +45,13 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('Name defined',function(){
+
+            for(let feed of allFeeds){
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            }
+         });
     });
 
 
